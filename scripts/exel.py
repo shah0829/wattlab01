84bd7e31ed02e52f4c665799bb439627b7b1481d
Shahnawaz <shah.bioinfo0829@gmail.com>
2021-08-17 18:56:05 +0100

    updated exel.py

--- scripts/exel.py
+++ scripts/exel.py
@@ -3,11 +3,18 @@ import pandas as pd
 import argparse
 
 
-def save_file(in_data1, in_data2, filename):
-    filename1 = "%s_ADH.csv" % filename
-    filename2 = "%s_ALP.csv" % filename
-    in_data1.to_csv(filename1, header=True)
-    in_data2.to_csv(filename2, header=True)
+def save_file(in_data, filename, cols):
+    # filename1 = "%s_ADH.csv" % filename
+    filename1 = filename_maker(filename, str(cols))
+    # filename2 = "%s_ALP.csv" % filename
+    # filename2 = filename_maker(filename)
+    in_data.to_csv(filename1, header=True)
+    # in_data2.to_csv(filename2, header=True)
+
+
+def filename_maker(filename, cols):
+    filename_full = "%s_set_%s.csv" % filename, cols
+    return filename_full
 
 
 if __name__ == '__main__':
@@ -17,17 +24,24 @@ if __name__ == '__main__':
     arguments = parser.parse_args()
 
     files2open = os.listdir(path=arguments.path)
-
-    for file in files2open:
-        # read csv file here
-
-        fileo = os.path.join(arguments.path, file)
-        data = pd.read_csv(fileo)
-        data_df = pd.DataFrame(data)
-        # select the column of data A D and H
-        data_ADH = data_df.iloc[:, [0, 3, 7]]
-        # select the column of data A L and P
-        data_ALP = data_df.iloc[:, [0, 11, 15]]
-        base = os.path.basename(fileo)
-        filename = os.path.splitext(base)
-        save_file(data_ADH, data_ALP, filename)
+    set_of_col = int(input("How many sets of column:"))
+    for i in range(set_of_col):
+        print(f"Enter the {i}th set col to select from the excel use , as separate:")
+        col_set = input().split(",")
+        for file in files2open:
+            try:
+                # read csv file here
+                print("reading %s file for processing..." % file)
+                fileo = os.path.join(arguments.path, file)
+                data = pd.read_csv(fileo)
+                data_df = pd.DataFrame(data)
+                # select the column of data A D and H
+                if len(data_df.columns) >= 15:
+                    data_sel = data_df.iloc[:, col_set]
+                    base = os.path.basename(fileo)
+                    filename = os.path.splitext(base)
+                    save_file(data_sel, filename, i)
+                else:
+                    print(f'File {file} doesnt contain columns set {i}')
+            except:
+                pass
